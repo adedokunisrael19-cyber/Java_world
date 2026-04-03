5ba6ef8d5fb9ea0922d85dc7f94f6d0afde30b3a
Adedokun Israel <adedokunisrael19@gmail.com>
2026-04-03 08:19:01 +0100

    Add independence.java

---
+++ Independence.java
@@ -0,0 +1,8 @@
+public class Independence {
+	
+	public static void main(String[] args){
+		
+		System.out.printf("Nigeria became idependent in the %d and is full of great minds. I am one of them, My name is %s and i weigh %d and I am a %S", 1960, "Issy", 85 , "M" );
+	}
+
+}
