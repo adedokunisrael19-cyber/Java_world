6093f5a72b73f1631bc82df987dbdacee20eb0d8
Adedokun Israel <adedokunisrael19@gmail.com>
2026-04-03 08:37:28 +0100

    Add Numbering.java

---
+++ Numbering.java
@@ -0,0 +1,19 @@
+public class Numbering {
+	
+	public static void main(String[] args){
+		Double fweight = 35.4;
+		Double sweight = 45.7;
+		Double tweight = 45.9;
+		Double rweight = 102.0;	
+		Double saweight = 40.23;
+		System.out.println(fweight + "kg");
+		System.out.println(sweight + "kg");
+		System.out.println(tweight + "kg");
+		System.out.println(rweight + "kg");
+		System.out.println(saweight + "kg");
+	}
+
+}
+
+
+
